f5efd8524369cc27b7fec472ebcf28352633f0fa
Anish A <anish@helloinfinity.com>
2013-02-16 19:00:50 +0530

    Refractoring - Stage 1(2)

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,10 @@
 <?php
-$con = mysql_connect($db_host, $db_user, $db_password);
-if (!$con) {
+require_once '../config.php';
+$connection = mysql_connect($db_host, $db_user, $db_password);
+if (!$connection) {
     die('Could not connect: ' . mysql_error());
 }
 
-mysql_select_db($db_name, $con);
+mysql_select_db($db_name, $connection);
 
 ?>
