7d2fa34abafca33fa1c854e67b760d874048a05f
Siddique Aboobacker <siddique@gmail.com>
2013-04-09 11:43:39 +0530

    test

--- test/test.php
+++ test/test.php
@@ -60,11 +60,11 @@ function helloinfinityCallAPI($method, $url, $data = false) {
    <?php  
                             
 foreach ($tldmostarray as $value) { ?>
-<input type="radio" name="tld" value="<?php echo $value; ?>"  onchange="this.form.submit();" /><?php echo $value; ?>
+<input type="radio" name="tld" value="<?php echo $value; ?>"  onclick="this.form.submit();" /><?php echo $value; ?>
                                       <br/>  
  <?php } 
  foreach ($tldmorearray as $value) { ?>
-<input type="radio" name="tld" value="<?php echo $value; ?>"  onchange="this.form.submit();"  /><?php echo $value; ?>
+<input type="radio" name="tld" value="<?php echo $value; ?>"  onclick="this.form.submit();"  /><?php echo $value; ?>
                                       <br/>  
  <?php } ?>
                                       
