34ad45801938e2373488e556efdece2355ba1943
Akhil Krishnan <akhilkrishnans.1@gmail.com>
2013-04-11 17:18:35 +0530

    Bug Fix - download image position issue

--- includes/downloads.php
+++ includes/downloads.php
@@ -1,36 +1,77 @@
 <div class="banner_outer">
-	<div style="float:left; width: 32%; text-align: left; padding:2%; margin-left:5%;">
+    <div class="dwnld_block">
         <h4>Browser</h4>
-        <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Firefox</span>
-        <br>         
-        <a href="http://www.google.com/chrome/" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Chrome</span>
-	</div>
+        <div class="single_dwnld_item">
+            <div>
+                <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Firefox</div>
+        </div>
+        
+        <div class="single_dwnld_item">
+            <div>
+                <a href="http://www.google.com/chrome/" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Chrome</div>        
+        </div>
+    </div>
 
     
-    <div style="float:left; width: 32%; text-align: left; padding:2%; margin-left:5%;">
+    <div class="dwnld_block">
         <h4>Tools</h4>
-        <a href="http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.62-installer.exe" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Putty</span>
-        <br>         
-        <a href="https://filezilla-project.org/download.php" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Filezilla</span>
-	</div>
+        <div class="single_dwnld_item">
+            <div>
+                <a href="http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.62-installer.exe" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Putty</div> 
+        </div>
+        
+        <div class="single_dwnld_item">
+            <div>
+                <a href="https://filezilla-project.org/download.php" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Filezilla</div>
+        </div>
+    </div>
     
-    <div style="float:left; width: 32%; text-align: left; padding:2%; margin-left:5%;">
+    <div class="dwnld_block">
         <h4>Mail Client</h4>
-        <a href="https://www.mozilla.org/EN/thunderbird/" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Mozilla Thunder bird</span>        
+        <div class="single_dwnld_item">
+            <div> 
+                <a href="https://www.mozilla.org/EN/thunderbird/" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Mozilla Thunder bird</div>        
 	</div>
+    </div>
     
-    <div style="float:left; width: 32%; text-align: left; padding:2%; margin-left:5%;">
+    <div class="dwnld_block">
         <h4>Email Configuration</h4>
-        <a href="uploads/configure_outlook.pdf" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>How to configure email on outlook</span>
-        <br>         
-        <a href="uploads/cloudamaze.pdf" target="_blank"><img src="images/dwnld.PNG" class="dwnld_img"></a>
-        <span>Advantages of Cloudamaze email</span>
-	</div>
+        <div class="single_dwnld_item">
+            <div> 
+                <a href="uploads/configure_outlook.pdf" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">How to configure email on outlook</div>
+        </div>         
+        <div class="single_dwnld_item">
+            <div> 
+                <a href="uploads/cloudamaze.pdf" target="_blank">
+                    <img src="images/dwnld.PNG" class="dwnld_img">
+                </a>
+            </div>
+            <div class="dwnld_item_name">Advantages of Cloudamaze email</div>
+        </div>
+    </div>
     
 </div>
