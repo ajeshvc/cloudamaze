477fe98b9c8e1b244a26a85438bd7b516e115e94
Siddique Aboobacker <siddique@gmail.com>
2013-04-12 18:08:31 +0530

    domain price list using ajax

--- includes/api/domainpricelisttest.php
+++ includes/api/domainpricelisttest.php
@@ -12,7 +12,7 @@
         var id=-1;
         setInterval(function(){
   // Call to your function that performs an ajax call...
- showdomainprice(id++); 
+ showdomainprice(id++) 
 }, 0.1 * 60 * 1000);
     }
 function showdomainprice(id)
@@ -42,7 +42,7 @@ xmlhttp.send();
 
 </script>
 </head>
-<body onload="repeate()">
+<body onload="repeat()">
 
 <form >
 
