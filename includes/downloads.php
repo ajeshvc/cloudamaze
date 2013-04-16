5b0b68498ba5817a7d1b289230a0040179d05201
Siddique Aboobacker <siddique@gmail.com>
2013-04-16 12:41:40 +0530

    downloads

--- includes/downloads.php
+++ includes/downloads.php
@@ -1,87 +1,106 @@
 <div class="banner_outer">
     <div class="dwnld_block">
         <h4>Browser</h4>
+      
         <div class="single_dwnld_item">
+               <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">
             <div>
-                <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">
+               
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+                
             </div>
             <div class="dwnld_item_name">Firefox</div>
+             </a>
         </div>
+            
         
         <div class="single_dwnld_item">
+            <a href="http://www.google.com/chrome/" target="_blank">
             <div>
-                <a href="http://www.google.com/chrome/" target="_blank">
+                
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+              
             </div>
-            <div class="dwnld_item_name">Chrome</div>        
-        </div>
+            <div class="dwnld_item_name">Chrome</div>     
+             </a>
+             
     </div>
 
     
     <div class="dwnld_block">
         <h4>Tools</h4>
         <div class="single_dwnld_item">
+             <a href="http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.62-installer.exe" target="_blank">
+       
             <div>
-                <a href="http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.62-installer.exe" target="_blank">
+                
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+               
             </div>
             <div class="dwnld_item_name">Putty</div> 
+             </a>
         </div>
+            
         
         <div class="single_dwnld_item">
+            <a href="https://filezilla-project.org/download.php" target="_blank">
             <div>
-                <a href="https://filezilla-project.org/download.php" target="_blank">
+                
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+               
             </div>
             <div class="dwnld_item_name">Filezilla</div>
+             </a>
         </div>
     </div>
     
     <div class="dwnld_block">
         <h4>Mail Client</h4>
         <div class="single_dwnld_item">
+            <a href="https://www.mozilla.org/EN/thunderbird/" target="_blank">
             <div> 
-                <a href="https://www.mozilla.org/EN/thunderbird/" target="_blank">
+                
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+               
             </div>
-            <div class="dwnld_item_name">Mozilla Thunder bird</div>        
+            <div class="dwnld_item_name">Mozilla Thunder bird</div>    
+             </a>
 	</div>
     </div>
     
     <div class="dwnld_block">
         <h4>Email Configuration</h4>
         <div class="single_dwnld_item">
+             <a href="uploads/configure_outlook.pdf" target="_blank">
             <div> 
-                <a href="uploads/configure_outlook.pdf" target="_blank">
+               
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+                
             </div>
             <div class="dwnld_item_name">How to configure email on outlook</div>
+        </a>
         </div>         
         <div class="single_dwnld_item">
+              <a href="uploads/cloudamaze_email_advantage.pdf" target="_blank">
             <div> 
-                <a href="uploads/cloudamaze_email_advantage.pdf" target="_blank">
+              
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+               
             </div>
             <div class="dwnld_item_name">Advantages of Cloudamaze email</div>
+         </a>
         </div>
     </div>
     <div class="dwnld_block">
         <h4>Hosting Plans</h4>
         <div class="single_dwnld_item">
+             <a href="uploads/web_plan.pdf" target="_blank">Hosting Plans
             <div> 
-                <a href="uploads/web_plan.pdf" target="_blank">Hosting Plans
+               
                     <img src="images/dwnld.PNG" class="dwnld_img">
-                </a>
+               
             </div>
-          
+           </a>
         </div>         
         
     </div>
