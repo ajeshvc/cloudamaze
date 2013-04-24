ce38ef2ebde3e98afa41a44fe4efc026f430b28e
Siddique Aboobacker <siddique@gmail.com>
2013-04-24 14:39:21 +0530

    refund page

--- includes/refund.php
+++ includes/refund.php
@@ -2,8 +2,7 @@
   <div class="banner_outer">
       
       <ul>
-          <li>Get 30 days money back on every shared hosting.
-          <ul><li>No questions asked</li></ul></li>
+          <li>Get 30 days money back on every shared hosting.(No questions asked)</li>
           <li>No other products hold money back guarantee</li>
       </ul>
      
