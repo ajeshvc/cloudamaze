24f7e742676f162a91f76bc3a7bc9990d45f00ed
Siddique Aboobacker <siddique@gmail.com>
2013-05-10 11:46:00 +0530

    support page

--- includes/support_ticket.php
+++ includes/support_ticket.php
@@ -3,7 +3,7 @@
 <!--<div id="content">-->
 <!--<div class="banner_outer">-->
  
-<iframe src="http://cloudamaze.com/support" id="frame" class="banner_outer">
+<iframe src="http://cloudamaze.com/support/index.php" id="frame" class="banner_outer">
 </iframe>
 
 
