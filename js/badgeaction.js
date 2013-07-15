dc3ccf222065f8914ae5f750482cff6c66115c65
Siddique Aboobacker <siddique@gmail.com>
2013-07-16 00:13:12 +0530

    badges j js

--- js/badgeaction.js
+++ js/badgeaction.js
@@ -20,11 +20,13 @@ function issameposition(l,t,divid){
    {
     var p = getleftposition(divid);
     var q = gettopposition(divid);
-    alert(p);
+    
     alert(l);
-    alert(t);
+    
+    alert(p+70);
     alert(q);
-    if(p+70==l && q-203==t){
+    alert(t+203);
+    if(p+70==l && t+203==q){
     flag=true;
     }
    }
